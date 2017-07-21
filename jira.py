0f60a853eb75d60af9c8f1f0ba3bf23fb5b61ff9
modlinski <modlinski@modlinski-GL72-6QC>
2017-07-21 19:30:46 +0200

    initial commit

---
+++ jira.py
@@ -0,0 +1,30 @@
+# !/usr/bin/env python
+# encoding: utf-8
+
+# Script to convert TCs docstring into Jira description.
+
+import re
+from copy import deepcopy
+
+
+def modify_docstring(text):
+    mod1 = text.replace('            Given', '** *Given*')
+    mod2 = mod1.replace('            When', '** *When*')
+    mod3 = mod2.replace('            Then', '** *Then*')
+    mod4 = mod3.replace('IntegrationTest/', '* IntegrationTest/')
+    mod5 = mod4.replace('        ', '')
+    return mod5
+
+with open("test_case.txt", "r") as test_case:
+    all_docstrings = re.findall(r'(?<=""")(?s).*?(?=""")', test_case.read())
+    all_docstrings_copy = deepcopy(all_docstrings)
+    for docstring in all_docstrings_copy:
+        if 'IntegrationTest/' not in docstring:
+            all_docstrings.remove(docstring)
+    jira = ''
+    for docstring in all_docstrings:
+        jira = ''.join((jira, modify_docstring(docstring)))
+    jira = ''.join(('*Test Cases:*\n\n', jira))
+
+with open("jira.txt", "w") as f:
+    f.write(jira)
